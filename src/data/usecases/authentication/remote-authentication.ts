f167082affa0b2db5b89eb5c9e61815487448401
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2025-08-31 15:15:41 -0400

    refactor: change imports to use modules

--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,10 +1,11 @@
-import { HttpPostClient } from "data/protocols/http/http-post-client";
-import { AuthenticationParams } from "domain/usecases/authentication";
+import { HttpPostClient } from "@/data/protocols/http/http-post-client";
+import { AuthenticationParams } from "@/domain/usecases/authentication";
 
 export class RemoteAuthentication {
   constructor (
     private readonly url: string,
-    private readonly httpClient: HttpPostClient) {}
+    private readonly httpClient: HttpPostClient
+  ) {}
 
   async auth (params: AuthenticationParams): Promise<void> {
     await this.httpClient.post({
