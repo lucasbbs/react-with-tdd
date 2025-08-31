ca199222466a397058c5ef6adbf1f38eb7dd8674
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2025-08-31 14:03:03 -0400

    refactor: set async function post method HttpPostClientSpy

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -2,7 +2,7 @@ import { HttpPostClient } from "data/protocols/http/http-post-client"
 
 export class HttpPostClientSpy implements HttpPostClient {
       url?: string
-      post(params: string): Promise<void> {
+      async post(params: string): Promise<void> {
         this.url = params;
         return Promise.resolve();
       }
